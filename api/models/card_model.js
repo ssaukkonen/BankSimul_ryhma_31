09a44aaf4dd1980d16b5aa6d3887cb1561df5322
joonatulisalo <t0tujo01@students.oamk.fi>
2021-04-06 12:03:58 +0300

    Kryptaus lisätty

--- api/models/card_model.js
+++ api/models/card_model.js
@@ -1,4 +1,7 @@
 const db = require('../database');
+const bcrypt = require('bcryptjs');
+
+const saltRounds=10;
 
 const card = {
   getById: function(id, callback) {
@@ -8,21 +11,23 @@ const card = {
     return db.query('select * from card', callback);
   },
   add: function(card, callback) {
+    bcrypt.hash(card.pin, saltRounds, function(err, hash) {
     return db.query(
       'insert into card (id_customer,id_account,pin) values(?,?,?)',
-      [card.id_customer, card.id_account, card.pin],
-      callback
-    );
+      [card.id_customer, card.id_account, hash],
+      callback)
+    });
   },
   delete: function(id, callback) {
     return db.query('delete from card where id_card=?', [id], callback);
   },
   update: function(id, card, callback) {
+    bcrypt.hash(user.pin, saltRounds, function(err, hash) {
     return db.query(
       'update card set id_customer=?,id_account=?,pin=? where id_card=?',
-      [card.id_customer, card.id_account, card.pin, id],
-      callback
-    );
+      [card.id_customer, card.id_account, hash, id],
+      callback)
+    });
   }
-};
+}
 module.exports = card;
