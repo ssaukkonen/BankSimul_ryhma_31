31556224f2adafd7915b3f91c7acd2fe7e8e8037
Samuli Saukkonen <ssaukkonen@gmail.com>
2021-04-09 10:43:41 +0300

    dllrestapin pohja

--- qt_frontend/ATM/nosto.cpp
+++ qt_frontend/ATM/nosto.cpp
@@ -6,9 +6,18 @@ nosto::nosto(QWidget *parent) :
     ui(new Ui::nosto)
 {
     ui->setupUi(this);
+    pengineAtmNosto = new engineatm;
+    connect(this,SIGNAL(SignalToEngineFromNosto(QString)),pengineAtmNosto,SLOT(receiveSignalfromNosto(QString)),Qt::QueuedConnection);
 }
 
 nosto::~nosto()
 {
     delete ui;
+    delete pengineAtmNosto;
+}
+
+void nosto::on_button20_clicked()
+{
+    amount="20";
+    emit SignalToEngineFromNosto(amount);
 }
